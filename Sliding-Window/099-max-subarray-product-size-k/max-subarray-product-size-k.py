84530749e07200372c61d8303c23a174285c629d
Sk Md Ashif Ali <36637666+ashif-ali@users.noreply.github.com>
2026-06-27 10:41:08 +0530

    Solved 'max subarray product size k' (python3)

--- Sliding-Window/099-max-subarray-product-size-k/max-subarray-product-size-k.py
+++ Sliding-Window/099-max-subarray-product-size-k/max-subarray-product-size-k.py
@@ -1,15 +1,8 @@
 def max_subarray_product_size_k(nums, k):
-  curr_prod = 1
-  
+  current_product = 1
   for i in range(0, k):
-    curr_prod *= nums[i]
+    current_product *= nums[i]
+  max_product = current_product
 
-  max_prod = curr_prod
-
-  for i in range(0, len(nums) - k):
-    curr_prod /= nums[i]
-    curr_prod *= nums[i + k]
-
-    max_prod = max(max_prod, curr_prod)
-  return max_prod
-  
+  for i in range(0, len(nums)-k):
+    
